b71f158a573a210bbe908632dbde1a4f121c89db
drivin77 <danielsrivin@gmail.com>
2014-07-10 15:33:41 -0700

    Update install_package.rb
    syntax fix

--- recipes/install_package.rb
+++ recipes/install_package.rb
@@ -8,6 +8,7 @@ rpm_file = "monit-5.5-1.el6.rf.x86_64.rpm"
 perform_install = true
 if File.exists?(Chef::Config[:file_cache_path]/rpm_file)
   perform_install = false
+end
 
 remote_file "#{Chef::Config[:file_cache_path]}/#{rpm_file}" do
   source "http://pkgs.repoforge.org/monit/#{rpm_file}"
